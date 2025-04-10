7751fd7902df5da17e299879642ee550b8b22c6c
Fedi <fedi.ghalloussi@gmail.com>
2025-04-10 21:09:08 +0200

    improve responsiveness (not done)

--- frontend/src/components/common/CategoryCard.vue
+++ frontend/src/components/common/CategoryCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-      class="bg-blue-100 p-6 py-8 rounded-xl text-center transition-transform duration-300 shadow-md cursor-pointer hover:-translate-y-1 hover:shadow-lg min-w-[140px] max-w-[200px] w-full"
+      class="bg-blue-100 px-6 py-4 md:py-8 rounded-xl text-center transition-transform duration-300 shadow-md cursor-pointer hover:-translate-y-1 hover:shadow-lg min-w-[140px] max-w-[200px] w-full"
   >
     <div class="text-4xl text-blue-800 font-mono flex items-center justify-center min-h-[50px]">
       {{ category.icon }}
